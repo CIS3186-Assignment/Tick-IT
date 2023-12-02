f237cabd81a7a5a899daf5c4c89d547d38f052ed
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-12-02 14:52:59 +0100

    finished wallet :)

---
+++ components/WalletCard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Card } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { IconButton, MD3Colors } from "react-native-paper";
+
+const EventCard = ({ event, imageURL }) => {
+  const navigation = useNavigation();
+
+  const handleCardPress = () => {
+    navigation.navigate("EventDetails", { event });
+  };
+
+  const goToEventCreator = () => {
+    creator = event.creator
+    navigation.navigate("EventCreator", { creator });
+  }
+
+  return (
+    <TouchableOpacity onPress={handleCardPress}>
+      <Card style={styles.cardContent}>
+        <Card.Content style={styles.content}>
+          {event?.image && (
+            <Image
+              style={styles.image}
+              source={{ uri: event.image }}
+              resizeMode="contain"
+            />
+          )}
+          <View style={styles.textContainer}>
+            <Text style={styles.eventName}>{event?.name}</Text>
+            <View style={styles.infoContainer}>
+              <IconButton
+                icon="map-marker"
+                size={18}
+                style={styles.iconButton}
+                iconColor={MD3Colors.neutral100}
+              />
+              <Text style={styles.infoText}>{event?.location}</Text>
+            </View>
+            <View style={styles.infoContainer}>
+              <IconButton
+                icon="account"
+                size={18}
+                style={styles.iconButton}
+                iconColor={MD3Colors.neutral100}
+              />
+              <TouchableOpacity onPress={goToEventCreator}>
+                <Text style={styles.infoText}>{event?.creator?.name}</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.infoContainer}>
+              <IconButton
+                icon="currency-eur"
+                size={18}
+                style={styles.iconButton}
+                iconColor={MD3Colors.neutral100}
+              />
+              <Text style={styles.infoText}>{event?.price}</Text>
+            </View>
+          </View>
+        </Card.Content>
+      </Card>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContent: {
+    marginVertical: 15,
+    marginHorizontal: 0,
+    borderTopWidth: 0.3,
+    borderBottomWidth: 0.4,
+    borderColor: "white",
+    borderRadius: 0,
+    backgroundColor: "#253354",
+  },
+  content: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 240,
+  },
+  image: {
+    width: "45%",
+    height: "100%",
+    borderRadius: 10,
+    right: 5,
+    backgroundColor: "#aaa",
+  },
+  textContainer: {
+    flex: 1,
+    marginTop: 5,
+    marginLeft: 4,
+    marginRight: 5,
+    flexDirection: "column",
+  },
+  eventName: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "800",
+    marginBottom: 15,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+    marginLeft: -1,
+  },
+  iconButton: {
+    marginRight: -2,
+    marginLeft: -10,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 15,
+  },
+});
+
+export default EventCard;
--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,25 +1,83 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { SegmentedButtons } from 'react-native-paper';
 import BottomNavBar from '../components/BottomNavBar';
+import { STORAGE } from '../FirebaseConfig.js';
+import WalletCard from '../components/WalletCard.js';
+import { getCategories } from '../services/CategoriesService.js';
+import sampleEvents from '../sample_data/events';
+
 
 const Wallet = () => {
+  const [value, setValue] = React.useState('');
   return (
-    <View style={{ flex: 1, position: 'relative', backgroundColor: '#141414'}}>
-      <Text style={styles.test}>This is the Wallet Screen</Text>
-      <View style={{ flex: 1 }}>
-        {/* Your main content goes here */}
+    <View style={{ ...styles.container, backgroundColor: "#141414" }}>
+      <View>
+        <SegmentedButtons
+          value={value}
+          onValueChange={setValue}
+          style={styles.segmentedButtons}
+          buttons={[
+            {
+              value: 'upcoming',
+              label: 'Upcoming',
+            },
+            {
+              value: 'past',
+              label: 'Past',
+            },
+          ]}
+        />
       </View>
+      <FlatList
+        data={sampleEvents.events}
+        keyExtractor={(item) => item.id}
+        style={styles.eventCard}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <WalletCard event={item} imageURL={item.image} />
+        )}
+      />      
       <BottomNavBar currentScreen="Wallet" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  test: {
-    color: '#fff',
-    textAlign: 'center',
-    marginTop: '100%' 
-  }
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+  segmentedButtons: {
+    marginTop: 55,
+    marginHorizontal: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderRadius: 20,
+    backgroundColor: "#FFFFFF",
+  },
+  eventCard: {
+    marginBottom: 0,
+  },
+  filter: {
+    marginVertical: 0,
+    borderBottomWidth: 0.7,
+    borderColor: "white",
+  },
+  filter_items: {
+    marginVertical: 15,
+    marginHorizontal: 0,
+    paddingHorizontal: 20,
+  },
+  category_chips: {
+    marginRight: 5,
+    backgroundColor: "#ffff",
+    borderRadius: 20,
+  },
+  chips: {
+    marginRight: 20,
+    paddingRight: 10,
+  },
 });
 
 export default Wallet;

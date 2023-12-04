5e223bca1c98705524ced913a7550ac1a2fe4572
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-12-04 09:00:15 +0100

    signup and signin forms split

--- screens/Login.js
+++ screens/Login.js
@@ -1,73 +1,56 @@
+// LoginScreen.js
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, HelperText, Button } from 'react-native-paper';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, AuthError } from 'firebase/auth';
+import { useNavigation } from '@react-navigation/native';
 
-export default Login = () => {
+const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const emailHasErrors = () => {
-        const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-        return !pattern.test(email)
-    }
-
-    const passwordHasErrors = () => {
-        // TODO
-    }
+    const navigation = useNavigation();
 
     const signIn = async () => {
         try {
-            setLoading(true);
-            await signInWithEmailAndPassword(FIREBASE_AUTH, email, password).then((userCredential) => {
-                const user = userCredential.user;
-                console.log(user);
-            });
+            // Attempt to sign in
+            const userCredential = await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
+            const user = userCredential.user;
+            console.log(user);
         } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
+            // Handle different authentication errors
+            switch (error.code) {
+                case 'auth/invalid-login-credentials':
+                    setErrorMessage('Wrong Credentials: The email address and password do not match.');
+                    break;
+                default:
+                    console.log(error.code);
+                    setErrorMessage('Authentication Failed: An unexpected error occurred.');
+            }
         }
     };
-    
-    const signUp = async () => {
-        try {
-            setLoading(true);
-            await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password).then((userCredential) => {
-                const user = userCredential.user;
-                console.log(user);
-            });
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    }
 
     return (
         <View style={{ flex: 1, position: 'relative', backgroundColor: '#141414' }}>
             <View style={{ flex: 1 }}>
                 <TextInput label="Email" value={email} onChangeText={setEmail} />
                 <TextInput label="Password" secureTextEntry={true} value={password} onChangeText={setPassword} />
-                <>
-                    <Button onPress={signIn} textColor='#fff'>Sign In</Button>
-                    <Button onPress={signUp} textColor='#fff'>Sign Up </Button>
-
-                </>
+                {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+                <Button onPress={signIn} textColor='#fff'>Sign In</Button>
+                <Button onPress={() => navigation.navigate('Register')} textColor='#fff'>No account? Sign Up</Button>
             </View>
         </View>
     );
 };
 
-
-
 const styles = StyleSheet.create({
-    test: {
-        color: '#fff',
-        textAlign: 'center',
-        marginTop: '100%'
-    }
+    errorText: {
+        color: 'red',
+        marginTop: 10,
+        marginBottom: 10,
+    },
 });
 
+export default Login;
--- screens/Register.js
+++ screens/Register.js
@@ -1,42 +1,62 @@
-import React, {useState} from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, HelperText } from 'react-native-paper';
+// SignUpScreen.js
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { FIREBASE_AUTH } from '../FirebaseConfig';
+import { createUserWithEmailAndPassword,updateProfile } from 'firebase/auth';
 
 const Register = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const emailHasErrors = () => {
-        const pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-        return !pattern.test(email)
-    }
-
-    const passwordHasErrors = () => {
-        // TODO
-    }
+    const signUp = async () => {
+        try {
+            // Attempt to sign up
+            const userCredential = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
+            const user = userCredential.user;
+            await updateProfile(user, { displayName: name });
+            console.log(user);
+        } catch (error) {
+            // Handle different authentication errors
+            switch (error.code) {
+                case 'auth/invalid-email':
+                    setErrorMessage('Invalid Email: The email address is not valid.');
+                    break;
+                case 'auth/email-already-in-use':
+                    setErrorMessage('Email Already in Use: The email address is already associated with another account.');
+                    break;
+                case 'auth/weak-password':
+                    setErrorMessage('Weak Password: The password must be at least 6 characters long.');
+                    break;
+                default:
+                    console.log(error);
+                    setErrorMessage('Sign Up Failed: An unexpected error occurred.');
+            }
+        }
+    };
 
     return (
-        <View style={{ flex: 1, position: 'relative', backgroundColor: '#141414'}}>
-        <View style={{ flex: 1 }}>
-            <TextInput label="Email" value={email} onChangeText={setEmail} />
-            <HelperText type="error" visible={emailHasErrors()}>
-                Email address is invalid!
-            </HelperText>
-            <TextInput label="Password" value={password} onChangeText={setPassword} />
-            <HelperText type="error" visible={passwordHasErrors()}>
-                Password is invalid!
-            </HelperText>
-        </View>
+        <View style={{ flex: 1, position: 'relative', backgroundColor: '#141414' }}>
+            <View style={{ flex: 1 }}>
+                <TextInput label="Name" value={name} onChangeText={setName} />
+                <TextInput label="Email" value={email} onChangeText={setEmail} />
+                <TextInput label="Password" secureTextEntry={true} value={password} onChangeText={setPassword} />
+                {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+                <Button onPress={signUp} textColor='#fff'>Sign Up</Button>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-  test: {
-    color: '#fff',
-    textAlign: 'center',
-    marginTop: '100%' 
-  }
+    errorText: {
+        color: 'red',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    // ... your other styles
 });
 
 export default Register;

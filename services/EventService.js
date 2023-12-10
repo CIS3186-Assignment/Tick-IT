fea4b2d6c744832420d8a27c9ebef72248cc395b
wayne1512 <57416683+wayne1512@users.noreply.github.com>
2023-12-10 10:37:00 +0100

    async getting events

--- services/EventService.js
+++ services/EventService.js
@@ -1,62 +1,52 @@
 import { getDocs, collection, getDoc } from "firebase/firestore";
 import { FIRESTORE } from "../FirebaseConfig";
 
+const fetchEventTickets = async (eventTicketsCollection) => {
+  const eventTicketsCollectionDocs = await getDocs(eventTicketsCollection);
+
+  return Promise.all(
+    eventTicketsCollectionDocs.docs.map(async (eventTicketsDoc) => {
+      const ticket = {
+        ...eventTicketsDoc.data(),
+      };
+
+      ticket.name = (
+        await getDoc(ticket.TicketType).then((doc) => doc.data())
+      ).name;
+
+      return ticket;
+    })
+  );
+};
+
 export const getAllEvents = async () => {
   try {
     const eventsCollection = collection(FIRESTORE, "Events");
     const querySnapshot = await getDocs(eventsCollection);
 
-    const events = [];
-    const creatorPromises = [];
-    const categoryPromises = [];
-
-    for (const documentSnapshot of querySnapshot.docs) {
+    const eventPromises = querySnapshot.docs.map(async (documentSnapshot) => {
       const event = {
         id: documentSnapshot.id,
         ...documentSnapshot.data(),
       };
 
-      let eventTicketsCollection = collection(
+      console.log("loaded event");
+
+      const eventTicketsCollection = collection(
         FIRESTORE,
         documentSnapshot.ref.path,
         "EventTickets"
       );
 
-      let eventTickets = [];
+      const categoryPromise = getDoc(event.Category);
+      const creatorPromise = getDoc(event.EventCreator);
+      const ticketsPromise = fetchEventTickets(eventTicketsCollection);
 
-      let eventTicketsCollectionDocs = (await getDocs(eventTicketsCollection))
-        .docs;
-
-      for (let eventTicketsDoc of eventTicketsCollectionDocs) {
-        ticket = {
-          ...eventTicketsDoc.data(),
-        };
-
-        ticket.name = await (await getDoc(ticket.TicketType)).data().name;
-
-        eventTickets.push(ticket);
-      }
-
-      event.tickets = eventTickets;
-
-      const eventCategoryRef = event.Category;
-      const categoryPromise = getDoc(eventCategoryRef);
-      categoryPromises.push(categoryPromise);
-
-      const eventCreatorRef = event.EventCreator;
-      const creatorPromise = getDoc(eventCreatorRef);
-
-      creatorPromises.push(creatorPromise);
-
-      events.push(event);
-    }
-
-    const creatorDocs = await Promise.all([...creatorPromises]);
-    const categoryDocs = await Promise.all([...categoryPromises]);
-
-    for (let i = 0; i < categoryDocs.length; i++) {
-      const categoryDoc = categoryDocs[i];
-      const event = events[i];
+      const [categoryDoc, creatorDoc, eventTickets] = await Promise.all([
+        categoryPromise,
+        creatorPromise,
+        ticketsPromise,
+      ]);
 
       if (categoryDoc.exists()) {
         const categoryData = categoryDoc.data();
@@ -65,11 +55,6 @@ export const getAllEvents = async () => {
           ...categoryData,
         };
       }
-    }
-
-    for (let i = 0; i < creatorDocs.length; i++) {
-      const creatorDoc = creatorDocs[i];
-      const event = events[i];
 
       if (creatorDoc.exists()) {
         const creatorData = creatorDoc.data();
@@ -80,7 +65,13 @@ export const getAllEvents = async () => {
           phone: creatorData.contact_number,
         };
       }
-    }
+
+      event.tickets = eventTickets;
+
+      return event;
+    });
+
+    const events = await Promise.all(eventPromises);
 
     return events;
   } catch (error) {

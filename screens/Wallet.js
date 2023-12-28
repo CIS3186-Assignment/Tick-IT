a4de720ebda5b02311c8495cc42cfffa307f7f81
= <luigi.naudi@gmail.com>
2023-12-28 11:51:56 +0100

    Refactor WalletCard and Wallet components

--- screens/Wallet.js
+++ screens/Wallet.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, FlatList, Text } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { ActivityIndicator, IconButton, MD3Colors } from "react-native-paper";
 import { getUserBookedEvents } from "../services/WalletService";
 import { fetchImagesForEvents } from "../services/WalletService";
 import BottomNavBar from "../components/BottomNavBar";
@@ -9,17 +15,15 @@ import { FIREBASE_AUTH } from "../FirebaseConfig";
 
 const Wallet = () => {
   const [bookedEvents, setBookedEvents] = useState([]);
-  const [lastRenderedDate, setLastRenderedDate] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [numColumns, setNumColumns] = useState(2);
 
   useEffect(() => {
     const fetchBookedEvents = async () => {
       try {
-        const userId = FIREBASE_AUTH.currentUser.uid; // ToDo: Replace with the actual user ID
+        const userId = FIREBASE_AUTH.currentUser.uid;
         const events = await getUserBookedEvents(userId);
-
         const eventsWithImages = await fetchImagesForEvents(events);
-
         setBookedEvents(eventsWithImages);
       } catch (error) {
         console.error("Error fetching booked events:", error);
@@ -27,10 +31,20 @@ const Wallet = () => {
         setIsLoading(false);
       }
     };
-
+  
     fetchBookedEvents();
   }, []);
-
+  
+  const renderWalletCard = ({ item }) => (
+    <TouchableOpacity
+      key={item.id}
+      onPress={() => handleCardPress(item)}
+      style={styles.cardContainer}
+    >
+      <WalletCard event={{}} imageURL={item.imageURL || ""} ticket={item} />
+    </TouchableOpacity>
+  );
+  
   if (isLoading) {
     return (
       <View style={styles.loadingContainer}>
@@ -38,63 +52,18 @@ const Wallet = () => {
       </View>
     );
   }
-
+  
   return (
-    <View style={{ ...styles.container, backgroundColor: "#141414" }}>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Your Tickets</Text>
       <FlatList
-        data={bookedEvents}
-        keyExtractor={(item) => item.id}
-        style={styles.eventCard}
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <>
-            {item.eventDetails.map((ticket, index) => {
-              const imageURL = ticket.imageURL || "";
-              const eventName = ticket.eventDetails?.name || "Event Name";
-              const eventCreatorName =
-                ticket.eventDetails?.eventCreator?.name || "Event Creator";
-              const eventDescription =
-                ticket.eventDetails?.description || "Event Description";
-              const eventLocation =
-                ticket.eventDetails?.location_name || "Event Location";
-
-              const eventDatetime = ticket.eventDetails?.date?.seconds || 0;
-
-              const dateObject = new Date(eventDatetime * 1000);
-
-              const formattedDate = `${dateObject.toLocaleDateString()} ${dateObject.toLocaleTimeString()}`;
-
-              const showDate = lastRenderedDate !== formattedDate;
-
-              if (showDate) {
-                setLastRenderedDate(formattedDate);
-                return <Text style={styles.dateText}>{formattedDate}</Text>;
-              }
-
-              return (
-                <WalletCard
-                  key={ticket.id}
-                  event={{
-                    name: eventName,
-                    eventDetails: {
-                      eventCreator: {
-                        name: eventCreatorName,
-                      },
-                    },
-                    description: eventDescription,
-                    location: eventLocation,
-                    datetime: formattedDate,
-                    price: ticket.price || "Event Price",
-                  }}
-                  imageURL={imageURL}
-                  ticket={ticket}
-                />
-              );
-            })}
-          </>
-        )}
+        data={bookedEvents.flatMap(event => event.eventDetails)}
+        style={styles.imageGrid}
+        renderItem={renderWalletCard}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
       />
-      <BottomNavBar currentScreen="Wallet" />
+      <BottomNavBar currentScreen="Wallet"/>
     </View>
   );
 };
@@ -102,23 +71,44 @@ const Wallet = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position: "relative",
+    backgroundColor: "#141414",
+    paddingTop: 30,
   },
-  eventCard: {
-    marginBottom: 0,
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 10,
   },
-  dateText: {
-    color: "white",
+  headerText: {
+    color: "#FFFFFF",
+    fontSize: 34,
+    fontWeight: "bold",
     textAlign: "center",
-    fontSize: 18,
-    fontWeight: "800",
-    marginBottom: 15,
+    marginVertical: 10,
+  },
+  imageGrid: {
+    marginHorizontal: 10,
   },
   loadingContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#141414",
+  },
+  rowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 5,
+  },
+  cardContainer: {
+    flex: 1,
+    width: "50%", 
+  },
+  toggleButton: {
+    backgroundColor: "#253354",
+    borderRadius: 15,
+    right: 10,
   },
 });
 

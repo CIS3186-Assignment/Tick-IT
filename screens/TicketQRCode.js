b0f8d55bfe98461a3b998e0a80a9bfbc8ba66470
Rianne Azzopardi <rianneazzopardi@gmail.com>
2024-01-16 20:46:05 +0100

    cleaned up import

--- screens/TicketQRCode.js
+++ screens/TicketQRCode.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  View,
-  Image,
-  StyleSheet,
-  FlatList,
-  Text,
-  Linking,
-  TouchableOpacity
-} from "react-native";
+import {View,Image,StyleSheet,FlatList,Text,Linking,TouchableOpacity} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { IconButton } from "react-native-paper";
 import TopAppBar from "../components/TopAppBar";
